6584e9e1ac12d8b812a7e24238f7a38f404776ed
sumairas <sumairas@pcvm5-23.lan.sdn.uky.edu>
2016-03-12 17:49:49 -0500

    temP

--- myapp.py
+++ myapp.py
@@ -5,6 +5,7 @@ import time
 import untangle
 import time
 import re
+import httplib
 import networkx as nx
 
 #Declare variables
@@ -19,6 +20,42 @@ Network = nx.Graph()
 Bandwidth_t1 = dict()
 Bandwidth_t2 = dict()
 BandwidthUsage = dict()
+
+#Declare static flowpusher class
+
+class StaticFlowPusher(object):
+ 
+    def __init__(self, server):
+        self.server = server
+ 
+    def get(self, data):
+        ret = self.rest_call({}, 'GET')
+        return json.loads(ret[2])
+ 
+    def set(self, data):
+        ret = self.rest_call(data, 'POST')
+        return ret[0] == 200
+ 
+    def remove(self, objtype, data):
+        ret = self.rest_call(data, 'DELETE')
+        return ret[0] == 200
+ 
+    def rest_call(self, data, action):
+        path = '/wm/staticflowpusher/json'
+        headers = {
+            'Content-type': 'application/json',
+            'Accept': 'application/json',
+            }
+        body = json.dumps(data)
+        conn = httplib.HTTPConnection(self.server, 8080)
+        conn.request(action, path, body, headers)
+        response = conn.getresponse()
+        ret = (response.status, response.reason, response.read())
+        print ret
+        conn.close()
+        return ret
+
+pusher = StaticFlowPusher('128.163.232.72')
 
 # Get switch and link information from the controller
 
@@ -99,11 +136,50 @@ interswitchLinks = list(set(interswitchLinks))
 
 #print Network.nodes(data=True)
 								
-#for path in nx.all_simple_paths(Network, source='h2', target='h3'):
-#    print path
-print "Topology created"
 
-#get statistics
+print "Topology created"
+count = 0
+
+def generate_rule_for_path(path,sourceIP,destIP):
+	for i,p in enumerate(path):
+		if "s" in p:# or i == len(path)-1:
+			dpid=Network.node[p]['DPID']
+			print "node:%s",p
+			if (i+1) < len(path):
+				nextHop = path[i+1]
+				print "nexthop:%s",nextHop
+			for i,interface in enumerate(Network.node[p]['interfaces']):
+				L = Network.node[p]['interfaces'][i]['link']
+				for i2,interface2 in enumerate(Network.node[nextHop]['interfaces']):
+					print "nexthop interface:%s",Network.node[nextHop]['interfaces'][i2]['link']
+					if (Network.node[nextHop]['interfaces'][i2]['link']== L):
+						finalLink=L
+#						print "node interface:%s",L
+						print "final Link:%s", finalLink
+							
+'''
+
+	flow = {
+    		'switch':dpid,
+    		"name":"flow_" + count,
+		"cookie" : "0"
+    		"priority":"3",
+		"eth_type" : "0x0x800", 
+    		"ipv4_src" : sourceIP,
+		"ipv4_dst" : destIP,
+    		"active":"true",
+    		"actions":"set_eth_src=02:cd:3b:33:87:63,set_eth_dst=02:43:58:c8:59:43,output=2"
+    		}
+'''
+for path in nx.all_simple_paths(Network, source='h2', target='h3'):
+        if path not in nx.shortest_path(Network, source='h2', target='h3'):
+		sendPath = path
+
+sourceip=Network.node['h2']['interfaces'][0]['IP']
+destip=Network.node['h3']['interfaces'][0]['IP']
+generate_rule_for_path(sendPath,sourceip,destip)
+'''
+#---------get statistics-----------#
 
 print "Preparing for querying statistics"
 time.sleep(5)
@@ -158,7 +234,7 @@ while True:
                                 if (P <> 'local') and (L in interswitchLinks):
                                 	BandwidthUsage[Network.node[nodes]['DPID']+P] = Bandwidth_t2[Network.node[nodes]['DPID']+P] - Bandwidth_t1[Network.node[nodes]['DPID']+P]
 
-	print BandwidthUsage	
-#add route
-
+#	print BandwidthUsage	
 	
+'''
+#-------Poll switches to see which flows are active-----#	
